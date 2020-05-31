98e8eb5a37637a05708627f6d78ee63364f22ce9
Katarzyna Woźna <62303442+katarzynawozna@users.noreply.github.com>
2020-05-31 15:27:37 +0200

    wersja Alfa

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 
@@ -8,4 +9,14 @@ class Profile(models.Model):
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
-        return f'Użytkownik {self.user.username}'
+        return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
